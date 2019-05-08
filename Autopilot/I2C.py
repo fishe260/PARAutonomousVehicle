aef50ed6b574b4cc7ac0646440bc0af2880ee02e
Aidan Fisher <fishe260@purdue.edu>
2019-05-08 13:05:44 -0400

    Created I2C.pi
    Condensed I2C functions - Copied from Parker Crowell

---
+++ Autopilot/I2C.py
@@ -0,0 +1,34 @@
+'''
+I2C
+Written by Parker Crowell
+RPi to Aruduino I2C Functions
+Wiring
+RPi - Arduino Uno
+Pin 2 SDA - Pin A4
+Pin 3 SDL - Pin A5
+'''
+
+from smbus2 import SMBusWrapper #
+
+address = 0x04 #Address of the connected Arduino
+channel = 1 #channel 1 for Rpi3
+
+def writeData(value): #Writes Bytes/ints
+	with SMBusWrapper(channel) as bus:
+		bus.write_byte_data(address,0,value) #Write value to address with 0 offset
+	return True
+	
+def writeBlock(value): #Writes Arrays
+	with SMBusWrapper(channel) as bus:
+		bus.write_i2c_block_data(address,0,value) #Write array value to address with 0 offset
+	return True
+	
+def readData(): #Reads Bytes/ints
+	with SMBusWrapper(channel) as bus:
+		value = bus.read_byte_data(address,0)
+	return value
+	
+def readBlock(size): #Reads Arrays of size
+	with SMBusWrapper(channel) as bus:
+		value = bus.read_i2c_block_data(address,0,size)
+	return value
