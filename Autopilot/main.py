07a23a1ec2dd39fb1db904e0fa6b91b7f6fa3016
Aidan Fisher <fishe260@purdue.edu>
2019-05-07 21:11:49 -0400

    Update main.py
    Fixed documentation

--- Autopilot/main.py
+++ Autopilot/main.py
@@ -16,14 +16,13 @@ class Window(object):
 
     def __init__(self, y1, y2, x, m=100, tolerance=50):
         """
-        Initialises a window object.
-        Parameters
-        ----------
-        y1          : Top y axis coordinate of the window rect.
-        y2          : Bottom y axis coordinate of the window rect.
-        x           : X coordinate of the center of the window rect
-        m           : X axis span, e.g. window rect width would be m*2..
-        tolerance   : Min number of pixels we need to detect within a window in order to adjust its x coordinate.
+        Description: Initialises a window object.
+        Inputs: y1          : Top y axis coordinate of the window rect.
+                y2          : Bottom y axis coordinate of the window rect.
+                x           : X coordinate of the center of the window rect
+                m           : X axis span, e.g. window rect width would be m*2..
+                tolerance   : Min number of pixels we need to detect within a window in order to adjust its x coordinate.
+        Outputs: N/A 
         """
         self.x = x
         self.mean_x = x
@@ -34,20 +33,9 @@ class Window(object):
 
     def pixels_in(self, nonzero, x=None):
         """
-        Returns indices of the pixels in `nonzero` that are located within this window.
-        Notes
-        -----
-        Since this looks a bit tricky, I will go into a bit more detail. `nonzero` contains two arrays of coordinates
-        of non-zero pixels. Say, there were 50 non-zero pixels in the image and `nonzero` would contain two arrays of
-        shape (50, ) with x and y coordinates of those pixels respectively. What we return here is a array of indices
-        within those 50 that are located inside this window. Basically the result would be a 1-dimensional array of
-        ints in the [0, 49] range with a size of less than 50.
-        Parameters
-        ----------
-        nonzero : Coordinates of the non-zero pixels in the image.
-        Returns
-        -------
-        Array of indices of the pixels within this window.
+        Description: Returns indices of the pixels in `nonzero` that are located within this window.
+        Inputs: nonzero : coordinates where gradient/color mask is true
+        Outputs: Array of indices of the pixels within this window.
         """
         if x is not None:
             self.x = x
@@ -64,10 +52,9 @@ class Window(object):
 
     def coordinates(self):
         """
-        Returns coordinates of the bounding rect.
-        Returns
-        -------
-        Tuple of ((x1, y1), (x2, y2))
+        Description: Returns coordinates of the bounding rect.
+        Inputs: N/A
+        Outputs: Tuple of ((x1, y1), (x2, y2))
         """
         return ((self.x - self.m, self.y1), (self.x + self.m, self.y2))
 
