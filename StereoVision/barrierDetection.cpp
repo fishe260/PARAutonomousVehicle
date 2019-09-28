67213a8c44a8dcb7a73244fedd2f679f0f7124cb
fishe260 <fishe260@purdue.edu>
2019-09-28 13:42:22 -0400

    Updated barrierDetection.cpp

--- StereoVision/barrierDetection.cpp
+++ StereoVision/barrierDetection.cpp
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
-#include <opencv2/calib3d.hpp>
 
+using namespace std;
 using namespace cv;
 
-Mat barrierDetecton(Mat disparity)
+/*
+ detectObjs
+ Accepts: Mat disparity: a stereo disparity image
+          int areaFilter: the minimum area to filter by (aka remove noise while detecting blobs)
+ Returns: vector<Keypoint> keypoints: all keypoints detected by the blob detector
+ */
+vector<KeyPoint> detectObjs(Mat disparity, int areaFilter)
 {
-  // 
+    // Set parameters for the blob detector
+    // TODO: Find good parameters
+    SimpleBlobDetector::Params blobDetectorParameters;
+    
+    // Enable filtering by area, allowing us to remove noise
+    blobDetectorParameters.filterByArea = true;
+    blobDetectorParameters.minArea = areaFilter;
+    
+    // Instantiate the blob detector with parameters enabled
+    Ptr<SimpleBlobDetector> blobDetector = SimpleBlobDetector::create(blobDetectorParameters);
+    
+    // Detect keypoints (where blobs are)
+    vector<KeyPoint> keypoints; // Will hold all keypoints detected
+    blobDetector->detect(disparity, keypoints);
+    
+    return keypoints;
 }
 
 Mat computeDisparity(Mat frameL, Mat frameR, std::string filename)
@@ -19,7 +40,7 @@ Mat computeDisparity(Mat frameL, Mat frameR, std::string filename)
   // TODO: Filters, Parameter settings, import calibration parameters, etc.
   
   // Compute the stereo disparity and store it in disparity
-  *stereoImage.compute(frameL, frameR, disparity);
+  stereoImage->compute(frameL, frameR, disparity);
 
   return disparity;
 }
